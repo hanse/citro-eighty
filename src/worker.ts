fb290e64a3938c327775105ba2a15e991785f344
Hans-Kristian Koren <hanse@koren.im>
2025-10-17 16:57:35 +0200

    Upgrade deps

--- src/worker.ts
+++ src/worker.ts
@@ -4,12 +4,14 @@ import {
   useJobId,
 } from '@devmoods/express-extras';
 
-import { jobs } from './api/config.js';
+import { jobs, redis } from './api/config.js';
 import * as appJobs from './api/jobs.js';
 
 setGlobalLoggerContext(() => ({
   jobId: useJobId(),
 }));
+
+await redis.connect();
 
 const worker = await createFaktoryWorker({
   jobs: Object.values(appJobs),
@@ -17,3 +19,4 @@ const worker = await createFaktoryWorker({
 });
 
 await worker.start();
+await redis.quit();
